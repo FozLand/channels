30a751aaee1b75eeb25da5db1edf20c310117a55
Tucker Bamberg <mootpoint23@gmail.com>
2016-11-18 19:23:09 -0500

    Minor Formatting Corrections

--- chatcommands.lua
+++ chatcommands.lua
@@ -1,5 +1,5 @@
-core.register_chatcommand("chls", {
-	description = "List players on channel",
+core.register_chatcommand('chls', {
+	description = 'List players on channel',
 	privs = {
 		interact = true,
 		shout = true
@@ -9,9 +9,9 @@ core.register_chatcommand("chls", {
 	end,
 })
 
-core.register_chatcommand("join", {
-	params = "<channel name>",
-	description = "join a channel",
+core.register_chatcommand('join', {
+	params = '<channel name>',
+	description = 'join a channel',
 	privs = {
 		interact = true,
 		shout = true
@@ -47,8 +47,8 @@ minetest.register_chatcommand('fjoin', {
 		end
 	end,
 })
-core.register_chatcommand("leave", {
-	description = "leave the channel",
+core.register_chatcommand('leave', {
+	description = 'leave the channel',
 	privs = {
 		interact = true,
 		shout = true
@@ -58,38 +58,38 @@ core.register_chatcommand("leave", {
 	end,
 })
 --[[
-minetest.register_chatcommand("channel", {
-	description = "Manages chat channels",
+minetest.register_chatcommand('channel', {
+	description = 'Manages chat channels',
 	privs = {
 		interact = true,
 		shout = true
 	},
 	func = function(name, param)
-		if param == "" then
-			minetest.chat_send_player(name, "Online players: /channel online")
-			minetest.chat_send_player(name, "Join/switch:    /channel set <channel>")
-			minetest.chat_send_player(name, "Leave channel:  /channel leave")
+		if param == '' then
+			minetest.chat_send_player(name, 'Online players: /channel online')
+			minetest.chat_send_player(name, 'Join/switch:    /channel set <channel>')
+			minetest.chat_send_player(name, 'Leave channel:  /channel leave')
 			return
-		elseif param == "online" then
+		elseif param == 'online' then
 			channels.command_online(name)
 			return
-		elseif param == "leave" then
+		elseif param == 'leave' then
 			channels.command_leave(name)
 			return
 		end
-		local args = param:split(" ")
-		if args[1] == "set" then
+		local args = param:split(' ')
+		if args[1] == 'set' then
 			if #args >= 2 then
 				 channels.command_set(name, args[2])
 				 return
 			end
 		end
-		minetest.chat_send_player(name, "Error: Please check again '/channel' for correct usage.")
+		minetest.chat_send_player(name, 'Error: Please check again '/channel' for correct usage.')
 	end,
 })
 --]]
 function channels.say_chat(name, message, channel)
-	minetest.log("action", "CHAT: "..message)
+	minetest.log('action', 'CHAT: '..message)
 	for k,v in pairs(channels.players) do
 		if v == channel then --and k ~= name then
 			minetest.chat_send_player(k, message)
@@ -99,11 +99,11 @@ end
 
 function channels.command_online(name)
 	local channel = channels.players[name]
-	local players = "You"
+	local players = 'You'
 	if channel then
 		for k,v in pairs(channels.players) do
 			if v == channel and k ~= name then
-				players = players..", "..k
+				players = players..', '..k
 			end
 		end
 	else
@@ -111,34 +111,34 @@ function channels.command_online(name)
 		for _,player in ipairs(oplayers) do
 			local p_name = player:get_player_name()
 			if not channels.players[p_name] and p_name ~= name then
-				players = players..", "..p_name
+				players = players..', '..p_name
 			end
 		end
 		return
 	end
 	
-	minetest.chat_send_player(name, "Online players in this channel: "..players)
+	minetest.chat_send_player(name, 'Online players in this channel: '..players)
 end
 
 function channels.command_set(name, param)
-	if param == "" then
-		minetest.chat_send_player(name, "Error: Empty channel name")
+	if param == '' then
+		minetest.chat_send_player(name, 'Error: Empty channel name')
 		return
 	end
 	
 	local channel_old = channels.players[name]
 	if channel_old then
 		if channel_old == param then
-			minetest.chat_send_player(name, "Error: You are already in this channel")
+			minetest.chat_send_player(name, 'Error: You are already in this channel')
 			return
 		end
-		channels.say_chat(name, "# "..name.." left channel "..channel_old, channel_old)
+		channels.say_chat(name, '# '..name..' left channel '..channel_old, channel_old)
 	else
 		local oplayers = minetest.get_connected_players()
 		for _,player in ipairs(oplayers) do
 			local p_name = player:get_player_name()
 			if not channels.players[p_name] and p_name ~= name then
-				minetest.chat_send_player(p_name, "# "..name.." left the global chat")
+				minetest.chat_send_player(p_name, '# '..name..' left the global chat')
 			end
 		end
 	end
@@ -154,15 +154,15 @@ function channels.command_set(name, param)
 	
 	channels.players[name] = param
 	channels.huds[name] = player:hud_add({
-		hud_elem_type	= "text",
-		name		= "Channel",
+		hud_elem_type	= 'text',
+		name		= 'Channel',
 		number		= 0xFFFFFF,
 		position	= {x = 0.6, y = 0.03},
-		text		= "Channel: "..param,
+		text		= 'Channel: '..param,
 		scale		= {x = 200,y = 25},
 		alignment	= {x = 0, y = 0},
 	})
-	channels.say_chat("", "# "..name.." joined channel "..param, param)
+	channels.say_chat('', '# '..name..' joined channel '..param, param)
 	channels.command_online(name)
 end
 
@@ -175,13 +175,13 @@ function channels.command_leave(name)
 	end
 	
 	if not (channels.players[name] and channels.huds[name]) then
-		minetest.chat_send_player(name, "Please join a channel first to leave it")
+		minetest.chat_send_player(name, 'Please join a channel first to leave it')
 		return
 	end
 	
 	if channels.players[name] then
 		local channel = channels.players[name]
-		channels.say_chat("", "# "..name.." left channel "..channel, channel)
+		channels.say_chat('', '# '..name..' left channel '..channel, channel)
 		channels.players[name] = nil
 	end
 	
